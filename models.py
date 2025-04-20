3841bae15ac3401b85f0e4238bea0e73f5a9095a
Łukasz Walczak <75444051+CrassusXY@users.noreply.github.com>
2025-04-20 11:07:24 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,14 +1,11 @@
-from sqlalchemy import Column, Integer, String, DateTime, JSON
+from sqlalchemy import Column, Integer, Text, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
 Base = declarative_base()
 
-class DeviceMessage(Base):
-    __tablename__ = "device_messages"
-
+class Message(Base):
+    __tablename__ = "messages"
     id = Column(Integer, primary_key=True)
-    device_id = Column(String, nullable=True)
-    topic = Column(String, nullable=True)
-    payload = Column(JSON, nullable=False)
+    raw_data = Column(Text, nullable=False)
     timestamp = Column(DateTime, default=datetime.utcnow)
